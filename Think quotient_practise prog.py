4561d93f45a1b67464753cd1f7cf9a0c011ecc89
kajolpokale <128905525+kajolpokale@users.noreply.github.com>
2023-03-25 18:09:06 +0530

    Add files via upload

---
+++ Think quotient_practise prog.py
@@ -0,0 +1,1208 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Hello')
+
+
+# In[2]:
+
+
+num=int(input('Enter a number')
+print('The original value is,num)
+num=float(num) 
+print('The converted value is,num')            
+       
+
+
+# In[ ]:
+
+
+num=int(input('Enter a number'))
+print('The original value is',num)
+num=float(num) 
+print('The converted value is',num)
+
+
+# In[ ]:
+
+
+rad=float(input('Enter the value of radius:'))
+area=(3.14)*(rad**2)
+print('The area of circle with radius',rad,"is"area)        
+          
+
+
+# In[ ]:
+
+
+print('Hello,world!')
+
+
+# In[ ]:
+
+
+#print a==5 & a=High five
+a=5
+print("a=",5)
+a="High five"
+print("a=",a)
+
+
+# In[ ]:
+
+
+#budget problem
+#Limit=5000
+#expend-->[]
+#6-->4800
+#5100
+
+limit=int(input("Enter your credit limit:"))
+expend=[]
+sum=0
+while sum<limit:
+    amout=int(input("Enter the amout:"))
+    if amount<0:
+        break
+    else:
+        sum+=amount
+        expend.(amount)
+        
+print("Total amount is",sum)
+print("The purchase made is",expend)
+print("The amount left is",(sum-limit))
+
+        
+        
+
+
+# In[ ]:
+
+
+qual="BE"
+expe
+
+str1="My name is kajol i am training with TQ"
+str.replace("","")
+str1.
+# In[ ]:
+
+
+str1='123456'
+str1.isnumeric
+
+
+# In[ ]:
+
+
+password=input("Enter a password:")
+digit=[]
+space=[]
+specialchar=[]
+    for letter in password:
+        if letter:is digit()
+         digit.append(letter)   
+        elif letter.is space:
+            
+
+
+
+
+# In[ ]:
+
+
+#print a=5 and a=High five
+print("a=",5)
+a="High five"
+print("a=",a)
+
+
+# In[ ]:
+
+
+#add two oprands,x=6 and y=8
+x=6
+y=8
+print("x+y=",x+y)
+
+
+# In[ ]:
+
+
+#floor division (qootient)
+print("x//y=",x//y)
+
+
+# In[ ]:
+
+
+#assign
+a=10
+b=7
+a*=b
+print(a)
+
+
+# In[ ]:
+
+
+#comparison op
+a=7
+b=5
+print(a>b)
+
+
+# In[ ]:
+
+
+a=69
+b=89
+print(a>b)
+
+
+# In[ ]:
+
+
+a=5    #equal to op
+b=4
+print('a==b',a==b)
+
+print("a!=b",a!=b)
+
+print("a<=b",a<=b)
+
+print("a>=b",a>=b)
+
+#And op
+a=5
+b=6
+print(a>2)and(b>6)
+
+x=10
+y=8
+print(bin(x))
+print(bin(y))
+
+
+# In[ ]:
+
+
+#rad
+#print-->area of circle
+rad=int(input("Enter the value of radius"))
+area=(3.14)*(rad**2)
+print("The area of circle with radius",rad,"is"area)
+
+
+# In[ ]:
+
+
+#WAP to check number is positive,negative or zero(if,elif)
+num=int(input("Enter the number"))
+if num>0:
+    print("This is Positive number")
+elif num==0:
+    print("This is negative number")
+else:
+    print("This no is zero")
+
+
+# In[ ]:
+
+
+#WAP to check whether number is prime or not
+
+num=int(input("Enter the number"))
+if num%2!=0:
+    if num%3!=0:
+        if num%4!=0:
+            if num%5!=0
+               if num%6!=0
+                 if num%7=0
+                  
+                 else:
+                      print("it is a prime number")
+            else:
+                    print("not primr number")
+                   
+            else: 
+                 print("not a prime number")
+        else :
+              print("not a prime number")
+    else:
+    
+        print("not a prime number")
+else:
+    print("not a prime number",)
+                      
+             
+                        
+        
+        
+                    
+                
+                    
+                    
+      
+                                  
+             
+                                  
+  
+   
+   
+
+                     
+                
+                
+
+
+# In[ ]:
+
+
+#simple calculator is the exam
+num1=int
+num2=int
+print("1-add,2-sub,3-multiply,4-divide")
+choice=int(input("Enter a choice:"))
+if choice==1:
+    print(n1+n2)
+elif choice==2:
+    print(n1-n2)
+elif choice==3:
+    print(n1*n2)
+elif choice==4:
+    print(n1/n2)
+else
+    print("Enter a valid choice")
+
+
+# In[ ]:
+
+
+#loop
+#for loop
+num= int(input("Enter a number"))
+for i in range(0,num,2):
+    print(i)
+
+
+# In[ ]:
+
+
+num= int(input("Enter a number"))
+for i in range(1,num+1):
+    print(i)
+
+
+# In[ ]:
+
+
+#for loop example:factorial 
+num=int(input("Enter the number for which we want to have factorial:"))
+fact=1
+for i in range(1,num+1):
+    fact=fact*i
+    
+print(fact)    
+
+
+# In[ ]:
+
+
+num=int(input("Enter the number:"))
+
+fact=1
+
+if num<0:
+    print("sorry factorial,does not:")
+              
+
+
+# In[ ]:
+
+
+for i in range(1,10):
+    print(i)
+
+
+# In[ ]:
+
+
+x=41
+if x>10:
+    print("above 10")
+    if x>20:
+        print( "above 20")
+    else:
+        ("print not above 20")   #not run
+
+
+# In[ ]:
+
+
+x=int(input("Enter the number:"))
+if x>0:
+    print("x is positive")
+elif x==0:
+    print("x is zero")
+else:
+    print("x is negative")   #not run
+
+
+# In[ ]:
+
+
+marks=int(input("Enter a marks"))
+if marks>-90:
+     print("Grade A")
+elif marks>-75:
+    print("Grade B")
+elif marks>-60:
+    print("Grade c")
+elif marks>-35:
+    print("Grade D")
+else:
+    print("Grade F")  #not done
+
+
+# In[ ]:
+
+
+for i in range(5):
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(1,20,2):
+    print(i)
+
+
+# In[ ]:
+
+
+#Armstrong number 153
+n=int(input("Enter no"))
+a=n
+sum=0
+while n>0:
+    r=n%10
+    sum=sum+r**3
+    n//10
+if sum==a:
+    print("Armstrong no")
+else:
+    print("Not Armstrong no")
+    
+
+
+# In[ ]:
+
+
+Q2 print odd number from 521 to 229 using  loop #loop
+start=int(input("Enter the start of range"))
+end=int(input("Enter the end of range"))
+for i in range (229,521+1):
+    if i%2 !=0:
+        print(i)
+
+
+# In[ ]:
+
+
+a=5
+b=6
+print("a=",5)
+print("b=",6)
+
+
+# In[ ]:
+
+
+x=5
+y=6
+print("x+y=",x+y)
+
+
+# In[ ]:
+
+
+a=10
+b=6
+a*=b
+print(a)
+
+
+# In[ ]:
+
+
+a=5
+b=8
+a-=b
+print(a)
+
+
+# In[ ]:
+
+
+a=4
+b=4
+print("a==b=",a==b)
+
+
+# In[ ]:
+
+
+a=51
+b=6
+print("a>b=",a>b)
+
+
+# In[ ]:
+
+
+site_name="kajol"
+print(site_name)
+
+
+# In[ ]:
+
+
+a,b,c=3,"k",2.5
+print(a)
+print(b)
+print(c)
+
+
+# In[ ]:
+
+
+site1=site2="kajol"
+print("site1")
+print("site2")
+
+
+# In[ ]:
+
+
+num1=23
+print(num1,"is of type",type(num1))
+
+
+# In[ ]:
+
+
+a=9
+b=8
+if a>b:
+    print("a is greater than b")
+
+
+# In[ ]:
+
+
+x=1
+if x>0:
+    print("x is positive no")
+
+
+# In[ ]:
+
+
+x=43
+if x%2==0:
+    print("x is even number")
+else:
+    print("x is odd number")
+
+
+# In[ ]:
+
+
+a=200
+b=56
+c=700
+if a>b and c>a:
+    print("both conditions are true")
+
+
+# In[ ]:
+
+
+a=200
+b=56
+c=700
+if a>b or a>c:
+    print("at least one conditions is true")
+    
+
+
+# In[ ]:
+
+
+x=int(input("Enter the number:"))
+if x>0:
+    print("x is positive")
+elif x==o:
+    print("x is zero")
+else:
+    print("x is negative")
+
+
+# In[ ]:
+
+
+def cube_root(n):
+    return n**0.3
+x=map(cube_root,list1)
+print(list1(n))
+
+
+# In[ ]:
+
+
+a=23
+b=456
+if b>a:
+    print("b is grater than a")
+
+
+# In[ ]:
+
+
+x=5
+if x%2==0:
+    print("x is positive")
+else:
+    print("x is negative")
+
+
+# In[ ]:
+
+
+a=33
+b=33
+if b<a:
+    print("b is graeter than a")
+else:
+    print(" a and b are equal")
+
+
+# In[ ]:
+
+
+x=int(input("Enter a value:"))
+if  x>0:
+    print("x is positive")
+elif x==0:
+    print("x is zero")
+else:
+    print("x is negative")
+
+
+# In[ ]:
+
+
+marks=int(input("Enter your marks:")) #elif
+if marks>=90:
+    print("Grade A")
+elif marks>=75:
+    print("Grade B")
+elif marks>=60:
+    print("Grade C")
+elif marks>=35:
+    print("Grade D")
+else:
+    print("Bad luck you have failed!")
+
+
+# In[ ]:
+
+
+#nested if
+marks=int(input("Enter your marks:"))
+if marks>=35:
+   print("you have passed")
+elif marks>=90:
+    print("Grade A")
+elif marks>=75:
+   print("Grade B")
+elif marks>=60:
+   print("Grade C")
+elif marks>=35:
+   print("Grade D")
+else:
+   print("Bad luck u have failed")
+
+
+# In[ ]:
+
+
+num1=int
+num2=int
+print("1+add,2-subtract,3-multiply,4-divide")
+choice=int(input("Enter a choice:")
+if choice==1:
+    print(n1+n2)
+elif choice==2:
+    print(n1-n2)
+elif choice==3:
+    print(n1*n2)
+elif choice==4:
+    print(n1/n2)
+else:
+    print("Enter a valid choice")
+          
+
+
+# In[ ]:
+
+
+
+count=1
+while count<=10:
+       print(count)
+       count+=1
+
+
+# In[ ]:
+
+
+print("number pattern")
+row=5
+for i in range(1,row+1,1):
+    for j in range(1,i+1):
+        print(j, end=" ")
+    print("")
+
+
+# In[ ]:
+
+
+sum=0
+n=int(input("Enter a number:"))
+for i in range(1,n+1,1):
+    sum+=i
+print("\n")
+print("sum is:",sum)
+
+
+# In[ ]:
+
+
+n=int(input("Enter a number:"))
+x=sum(range(1,n+1))
+print("sum is:",x)
+
+
+# In[ ]:
+
+
+#even number
+num=int(input("Enter the number"))
+if num%2==0:
+    print("even number")
+
+
+# In[ ]:
+
+
+#largest numbers of 3 numbers
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+if a>b and a>c:
+    print("a is largest")
+if b>a and b>c:
+    print("b is largest")
+if c>a and c>b:
+    print("c is largest")
+
+
+# In[ ]:
+
+
+#eligible to vote or not
+age=int(input("Enter the age:"))
+if age>=18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+
+# In[ ]:
+
+
+#to check no is even or not
+num=int(input("Enter the number:"))
+if num%2==0:
+    print("even number")
+else:
+    print("not even number")
+
+
+# In[ ]:
+
+
+int(2.13)
+
+
+# In[ ]:
+
+
+float(11)
+
+
+# In[ ]:
+
+
+ord("k")
+
+
+# In[ ]:
+
+
+for i in range(5):
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(1,7):
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(2,22,2):
+    print(i)
+
+
+# In[ ]:
+
+
+n=2
+for i in range(2,22,2):
+    product=n*i
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(10,0,-1):
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(20,0,-2):
+    print(i)
+
+
+# In[ ]:
+
+
+i=1
+n=5
+while i<=n:
+    print(i)
+    i=i+1
+
+
+# In[ ]:
+
+
+i=2
+n=20
+while i<=n:
+    print(i)
+    i+=1
+
+
+# In[ ]:
+
+
+num=8
+count=1
+fact=1
+while count<num+1:
+    fact=fact*count
+    count+=1
+print(fact)
+    
+
+
+# 
+
+# 
+
+# In[ ]:
+
+
+i=1
+while i<6:
+    print(i)
+    if i==3:
+        break
+    i=i+1
+
+
+# In[ ]:
+
+
+for i in range(1,40,1):
+    if i%5==0
+       continue
+    print(i
+
+
+# In[ ]:
+
+
+for i in range(1,10):
+    print(i)
+    if i%2==0:
+        pass
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(1,10):
+    print(i)
+    if i==5:
+        break
+    else:
+        print("reach till end success")
+
+
+# In[ ]:
+
+
+list1=[1,1,2,3,4,5,6,2]
+print(list1)
+print(len(list1))
+print(type(list1))
+
+
+# In[ ]:
+
+
+list1=['k','a','j','o','l']
+print(list1)
+print(list1[-1])
+
+
+# In[ ]:
+
+
+numbers=[2,4,6,8]
+print(numbers)
+numbers.append(5)
+print(numbers)
+del numbers[0:1]
+print(numbers)
+numbers.remove(6)
+print(numbers)
+del numbers[4]
+print(numbers)
+
+
+# In[ ]:
+
+
+my_tuple=('k','a','j','o','l')
+print(my_tuple)
+print(type(my_tuple))
+
+
+# In[ ]:
+
+
+s1={1,2,2,5,6}
+print(s1)
+s1.add(100)
+print(s1)
+s1.discard(5)
+print(s1)
+s1.pop()
+print(s1)
+
+
+# In[ ]:
+
+
+
+s3=s1.union(s2)
+print(s3)
+
+
+# In[ ]:
+
+
+s1={1,2,3}
+s2={4,5,6}
+s1.update(s2)
+print(s1)
+
+
+# In[ ]:
+
+
+a=5
+print("a=",5)
+a="High five"
+print("a=",a)
+
+
+# In[ ]:
+
+
+x=5
+y=4
+print("x+y=",x+y)
+print("x-y=",x-y)
+print("x*y=",x*y)
+print("x//y=",x//y)
+print("x%y=",x%y)
+print("x**y=",x**y)
+
+
+# In[ ]:
+
+
+print("5+8=",5+8)
+
+
+# In[ ]:
+
+
+a=10
+b=5
+a+=b
+print("a=",a)
+
+
+# In[ ]:
+
+
+a,b=10,12
+print(a==b)
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+print(a!=b)
+
+
+# In[ ]:
+
+
+a=12
+b=2
+a+=b
+print(a)
+
+
+# In[ ]:
+
+
+list1=["kajol","rahul","pavan"]
+print(list1)
+print("kajol" in list1)
+print("tanu" in list1)
+
+
+# In[ ]:
+
+
+a=10
+b=12
+print(a is b)
+print(a is not b)
+
+
+# In[ ]:
+
+
+a=10
+b=12
+print(a&b)
+print(a | b)
+print(a ^ b)
+print(~b)
+
+
+# In[ ]:
+
+
+for i in range(1,11):
+    if i==5:
+        break
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(1,11):
+    if i==5:
+        continue
+    print(i)
+        
+
+
+# In[ ]:
+
+
+for i in range(1,11):
+    pass
+
+
+# In[ ]:
+
+
+num1=5
+print( num1,"is of type",type(num1))
+
+
+# In[ ]:
+
+
+int(2.7)
+
+
+# In[ ]:
+
+
+float(9)
+
+
+# In[ ]:
+
+
+oct(9)
+
+
+# In[ ]:
+
+
+hex(8)
+
+
+# In[ ]:
+
+
+ord("k")
+
+
+# In[ ]:
+
+
+str(7)
+
+
+# In[ ]:
+
+
+a=33
+b=200
+if b>a:
+    print("b is greater than a")
+
+
+# In[ ]:
+
+
+x=33
+if x%2==0:
+    print("x is even")
+else:
+    print("x is odd")
+
+
+# In[ ]:
+
+
+for i in range(1,5):
+    print(i)
+
+
+# In[ ]:
+
+
+for i in range(2,22,2):
+    print(i)
+
+
+# In[ ]:
+
+
+i=1
+n=5
+while i<=n:
+    print(i)
+    i=i+1
+
+
+# In[ ]:
+
+
+list1=["kajol","rahul","tanu"]
+print(list1)
+
+
+# In[ ]:
+
+
+my_tuple=("mouse",[8,4,6],(1,2,3))
+print(my_tuple)
+
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[2]:
+
+
+a=5
+print("a=",5)
+a="high five"
+print("a=",a)
+
+
+# In[4]:
+
+
+a=5
+print("a=",a)
+a="high five"
+print("a=",a)
+
+
+# In[6]:
+
+
+x=14
+y=4
+print('x+y=',x+y)
+print('x/y=',x/y)
+
+
+# In[ ]:
+
+
+
+
