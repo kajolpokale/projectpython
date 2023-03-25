72568def4ac12dd91d4a6fd26540db1de625deb6
kajolpokale <128905525+kajolpokale@users.noreply.github.com>
2023-03-25 17:47:52 +0530

    Add files via upload

---
+++ Dictionary_practise.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+test1={1:45,2:67,4:57,5:23,5:37,6:37}
+print(test1)
+test1(len(test1))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# 
+# 
+
+# In[ ]:
+
+
+
+
